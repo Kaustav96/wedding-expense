671c8d7e91e7f76adf4835240d845b92bded60c8
Kaustav Banerjee <kaustavbanerjee96@gmail.com>
2026-03-23 00:22:03 +0530

    fix: explicitly assign seedAllMembers to window in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,10 @@ import './index.css'
 import App from './App.tsx'
 import { AuthProvider } from './context/AuthProvider.tsx'
 import { ExpenseProvider } from './context/ExpenseContext.tsx'
-import './scripts/seedMembers.ts'
+import { seedAllMembers } from './scripts/seedMembers.ts'
+
+// Make seeder available in browser DevTools console
+window.seedAllMembers = seedAllMembers
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
